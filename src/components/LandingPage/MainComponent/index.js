ebd5202ca353257be8da3fd8f15b6832656bbdd7
alecq031723 <144410466+alecq031723@users.noreply.github.com>
2025-09-15 13:00:19 -0400

    Applied framer motion for styling

--- src/components/LandingPage/MainComponent/index.js
+++ src/components/LandingPage/MainComponent/index.js
@@ -1,23 +1,65 @@
 import React from "react";
 import "./styles.css";
 import Button from "../../Common/Button";
+import iphone from "../../../assets/iphone.png";
+import gradient from "../../../assets/gradient.png";
+import { motion } from "framer-motion";
 
 function MainComponent() {
   return (
     <div className="flex-info">
       <div className="left-component">
-        <h1 className="track-crypto-heading">Track Crypto</h1>
-        <h1 className="real-time-heading">Real Time.</h1>
-        <p className="info-text">
+        <motion.h1
+          className="track-crypto-heading"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2 }}
+        >
+          Track Crypto
+        </motion.h1>
+        <motion.h1
+          className="real-time-heading"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2, delay: 0.5 }}
+        >
+          Real Time.
+        </motion.h1>
+        <motion.p
+          className="info-text"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2, delay: 0.75 }}
+        >
           Stay ahead of the market — track real-time crypto prices with live
           data from public APIs. Visit the dashboard to dive in!
-        </p>
-        <div className="btn-flex">
+        </motion.p>
+        <motion.div
+          className="btn-flex"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 2, delay: 1.5 }}
+        >
           <Button text={"Dashboard"} />
           <Button text={"Share"} outlined={true} />
-        </div>
+        </motion.div>
       </div>
-      <div>phone</div>
+      <div className="phone-container">
+        <motion.img
+          src={iphone}
+          alt="iPhone"
+          className="iphone"
+          initial={{ y: -10 }}
+          animate={{ y: 10 }}
+          transition={{
+            type: "smooth",
+            repeatType: "mirror",
+            duration: 2,
+            repeat: Infinity,
+          }}
+        />
+        <img src={gradient} alt="Gradient" className="gradient" />
+      </div>
     </div>
   );
 }
